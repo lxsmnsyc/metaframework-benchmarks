2621b5597ca8a57814b4ea29a71bcb1ddf1a69af
Alexis Munsayac <alexis.munsayac@gmail.com>
2023-08-15 00:31:59 +0800

    Add web-vitals measurement to `qwikcity`

---
+++ frameworks/qwikcity/src/lib/measure.ts
@@ -0,0 +1,54 @@
+import {
+  onCLS,
+  type CLSMetric,
+  type FCPMetric,
+  type FIDMetric,
+  type INPMetric,
+  type LCPMetric,
+  type TTFBMetric,
+  onFCP,
+  onFID,
+  onINP,
+  onLCP,
+  onTTFB,
+} from 'web-vitals';
+
+type WebVitalsMetric =
+  | CLSMetric
+  | FCPMetric
+  | FIDMetric
+  | INPMetric
+  | LCPMetric
+  | TTFBMetric;
+
+type WebVitalsCode = WebVitalsMetric['name'];
+
+export default function measure(): void {
+  const flags = new Set<WebVitalsCode>(['CLS', 'FCP', 'FID', 'INP', 'LCP', 'TTFB']);
+  const measurements: WebVitalsMetric[] = [];
+
+  function capture(value: WebVitalsMetric): void {
+    if (flags.size) {
+      console.log('CAPTURED', value.name);
+      flags.delete(value.name);
+      measurements.push(value);
+      if (flags.size === 0) {
+        const el = document.createElement('script');
+        el.id = 'web-vitals';
+        el.style.display = 'hidden';
+        el.innerText = JSON.stringify(measurements);
+        document.head.appendChild(el);
+      }
+    }
+  }
+
+  // On-load
+  onFCP(capture);
+  onTTFB(capture);
+  // input?
+  onLCP(capture);
+  onFID(capture);
+  // page visibility
+  onCLS(capture);
+  onINP(capture);
+}
--- frameworks/qwikcity/src/root.tsx
+++ frameworks/qwikcity/src/root.tsx
@@ -1,4 +1,4 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useVisibleTask$ } from '@builder.io/qwik';
 import {
   QwikCityProvider,
   RouterOutlet,
@@ -6,24 +6,30 @@ import {
 
 import './root.css';
 import Nav from './components/Nav';
+import measure from './lib/measure';
 
-export default component$(() => (
+export default component$(() => {
   /**
    * The root of a QwikCity site always start with the <QwikCityProvider> component,
    * immediately followed by the document's <head> and <body>.
    *
    * Don't remove the `<head>` and `<body>` elements.
    */
-  <QwikCityProvider>
-    <head>
-      <title>Hacker News Clone</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="description" content="Hacker News Clone" />
-    </head>
-    <body lang="en">
-      <Nav />
-      <RouterOutlet />
-    </body>
-  </QwikCityProvider>
-));
+  useVisibleTask$(() => {
+    measure();
+  });
+  return (
+    <QwikCityProvider>
+      <head>
+        <title>Hacker News Clone</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Hacker News Clone" />
+      </head>
+      <body lang="en">
+        <Nav />
+        <RouterOutlet />
+      </body>
+    </QwikCityProvider>
+  );
+});
