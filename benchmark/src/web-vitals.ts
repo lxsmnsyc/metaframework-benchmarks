d2b78e2cdf33682654c2e645e9ac5af18f9b537b
Alexis Munsayac <alexis.munsayac@gmail.com>
2023-08-22 22:22:07 +0800

    Add device emulation

--- benchmark/src/web-vitals.ts
+++ benchmark/src/web-vitals.ts
@@ -1,4 +1,4 @@
-import puppeteer from 'puppeteer';
+import * as puppeteer from 'puppeteer';
 import path from 'path';
 import { PAGE } from './constants';
 import { outputJson } from './fs-utils';
@@ -8,22 +8,22 @@ export default async function runWebVitals(framework: string): Promise<void> {
     headless: false,
   });
   const page = await browser.newPage();
-  // TODO setup mobile emulation
-  await page.setViewport({
-    width: 1366,
-    height: 768,
-  });
+  await page.emulate(puppeteer.KnownDevices['iPhone 13']);
   await page.goto(PAGE);
+  // Just to check if all measurements are captured.
   page.on('console', (msg): void => {
     Promise.all(msg.args().map(async (arg) => arg.jsonValue())).then(console.log, console.error);
   });
+  // When page loads, click the page
   const body = await page.waitForSelector('body');
   if (body) {
     await body.click();
   }
+  // Toggle page visibility by switching pages
   const dummyPage = await browser.newPage();
   await dummyPage.bringToFront();
   await page.bringToFront();
+  // Focus again
   if (body) {
     await body.click();
   }
